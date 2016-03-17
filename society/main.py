906bc99858bad621b573070794cbe3513d67e45b
JunkuiZhang <364772080@qq.com>
2016-03-17 13:16:58 +0800

    entities' ability added

--- society/main.py
+++ society/main.py
@@ -10,21 +10,23 @@ import csv
 import time
 
 # None for not saving files
-FILE_NAME = ""
+FILE_NAME = "test"
 
 LENGTH = 800
-ENTITIES = 4
-WIDTH = 400
+ENTITIES = 250
+WIDTH = 20
 
 # entity's behavior
-ENTITY_ABILITY = 2
+# -1 for random assigning
+ENTITY_ABILITY = -1
+RANDOM_RANGE = [1, 6]
 
 # -1 for no field upper bound
 FIELD_UPPER_BOUND = -1
 FIELD_LOWER_BOUND = 10
 
 MU = 100
-SIGMA = 60
+SIGMA = 50
 
 THRESHOLD_LOWER_BOUND = 50
 THRESHOLD_GROW = 1
@@ -40,13 +42,14 @@ class Worldgrid:
 		self.screen_size = (1600, length + 200)
 		self.width = width
 		self.po = int(self.length / self.width + 2)
+		self.field_greatest_product = list()
 
 	def generate_world_grid(self):
 		grid_length = self.po
 		grid_matrix = numpy.zeros((grid_length, grid_length))
 		center_index = grid_length // 2
-		center_semidiameter = grid_length // 16
-		medium_semidiameter = grid_length // 10
+		center_semidiameter = grid_length // 24
+		medium_semidiameter = grid_length // 8
 
 		def check_values(value):
 			if value < FIELD_LOWER_BOUND:
@@ -63,17 +66,20 @@ class Worldgrid:
 				if j == 0 or j == grid_length - 1:
 					continue
 				if abs(i - center_index) <= center_semidiameter and abs(j - center_index) <= center_semidiameter:
-					v = random.gauss(mu=MU + 300, sigma=SIGMA//3)
+					v = random.gauss(mu=10 * MU, sigma=SIGMA//3)
 					v = check_values(v)
 					grid_matrix[i, j] = v
+					self.field_greatest_product.append(v)
 				elif abs(i - center_index) <= medium_semidiameter and abs(j - center_index) <= medium_semidiameter:
-					v = random.gauss(mu=MU + 150, sigma=SIGMA//2)
+					v = random.gauss(mu=4.5 * MU, sigma=SIGMA//2)
 					v = check_values(v)
 					grid_matrix[i, j] = v
+					self.field_greatest_product.append(v)
 				else:
 					v = random.gauss(mu=MU, sigma=SIGMA)
 					v = check_values(v)
 					grid_matrix[i, j] = v
+					self.field_greatest_product.append(v)
 		return grid_matrix
 
 	def draw_grid(self):
@@ -97,7 +103,7 @@ class Worldgrid:
 		screen = pygame.display.set_mode(self.screen_size)
 		pygame.display.set_caption("Society")
 		grid_matrix = self.generate_world_grid()
-		po = self.po
+		max_product = max(self.field_greatest_product)
 
 		if FILE_NAME != "":
 			save_data = True
@@ -110,7 +116,7 @@ class Worldgrid:
 			w0 = csv.writer(f0)
 			w1 = csv.writer(f1)
 			w0.writerow(["Time", "Alive_num", "Richest", "Poorest", "Gini", "Threshold"])
-			w1.writerow(["Time", "ID", "Status", "Field_product", "Welfare", "Threshold"])
+			w1.writerow(["Time", "ID", "Ability", "Status", "Field_product", "Welfare", "Threshold"])
 		else:
 			save_data = False
 
@@ -137,16 +143,7 @@ class Worldgrid:
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					exit()
-			pygame.draw.rect(screen, (225, 225, 225), (0, 0, 1002, 1002), 2)
-			for n in range(po - 2):
-				pos_1 = n * self.width
-				pos_2 = n * self.width
-				pygame.draw.line(screen, (20, 20, 20), (0, pos_1), (1000, pos_2), 1)
-				pygame.draw.line(screen, (20, 20, 20), (pos_1, 0), (pos_2, 1000), 1)
-			entity.draw_entities(screen, self.width)
-			entity.entities_move(grid_matrix)
-			entity.draw_entities(screen, self.width)
-			pygame.display.update()
+			entity.draw_entities(screen, self.width, max_product, grid_matrix)
 
 			alive = 0
 			rich = 0
@@ -164,7 +161,7 @@ class Worldgrid:
 				if en["state"] == 1:
 					poor = min([en["welfare"], poor])
 				if save_data:
-					w1.writerow([life_time, en["id"], en["state"], grid_matrix[en["position"][0], en["position"][1]],
+					w1.writerow([life_time, en["id"], en["ability"], en["state"], grid_matrix[en["position"][0], en["position"][1]],
 						     en["welfare"], entity.threshold])
 			print("Alive now: %s" % alive)
 			print("Richest: %s" % rich)
@@ -178,6 +175,10 @@ class Worldgrid:
 			gini = get_gini(entity.entity_list)
 			print("The Gini index is %s" % gini)
 			print("=="*20)
+
+			entity.entities_move(grid_matrix)
+			pygame.display.update()
+
 			if save_data:
 				w0.writerow([life_time, alive, rich, poor, gini, entity.threshold])
 			if alive == 0:
@@ -202,11 +203,13 @@ class Entity:
 			self.entity_list.append({
 				"id": n,
 				"state": 1,
+				"ability": ENTITY_ABILITY,
 				"welfare": 0,
 				"life_length": 0,
 				"position": [0, 0]
 			})
 		self.generate_entities_position()
+		self.generate_entities_ability()
 
 	def generate_entities_position(self):
 		positions = list()
@@ -220,7 +223,26 @@ class Entity:
 			positions.append([row_num + 1, col_num + 1])
 			en["position"] = [row_num + 1, col_num + 1]
 
-	def draw_entities(self, screen, width):
+	def generate_entities_ability(self):
+		if ENTITY_ABILITY != -1:
+			pass
+		else:
+			half_value = RANDOM_RANGE[1] // 2
+			for en in self.entity_list:
+				index = random.gauss(0, 1)
+				if index >= 1.97:
+					# with a probability of 0.975
+					# only 2.5% of the entities will fall in this class
+					en["ability"] = RANDOM_RANGE[1]
+				elif index >= 0.2:
+					# with a probability of .58
+					# that's saying 40% of the entities will fall in this class
+					en["ability"] = random.choice(range(half_value + 1, RANDOM_RANGE[1], 1))
+				else:
+					# the rest 57.5% entities are normal
+					en["ability"] = random.choice(range(RANDOM_RANGE[0], half_value + 1, 1))
+
+	def draw_entities(self, screen, width, max_product, grid_matrix):
 		positions = list()
 		for en in self.entity_list:
 			if en["state"] == 0:
@@ -228,20 +250,23 @@ class Entity:
 			positions.append(en["position"])
 		for i in range(self.po):
 				for j in range(self.po):
+					v = grid_matrix[i, j] * 255 // max_product
+					pygame.draw.rect(screen, (v, v, v),
+							 ((j-1)*width, (i-1)*width, j*width, i*width))
 					if [i, j] in positions:
-						pygame.draw.rect(screen, (225, 225, 225),
-								 (j*width+1, i*width+1, (j+1)*width-1, (i+1)*width-1))
-					else:
-						pygame.draw.rect(screen, (0, 0, 0),
-								 (j*width+1, i*width+1, (j+1)*width-1, (i+1)*width-1))
+						pygame.draw.circle(screen, (234, 103, 83),
+								   ((j-1)*width+width//2, (i-1)*width+width//2), width//3)
 
 	def entities_move(self, grid_matrix):
 		world_time = 1
 		position_list = list()
+		total_time = -1
 		for en in self.entity_list:
 			if en["state"] == 0:
 				continue
 			position_list.append(en["position"])
+			if en["life_length"] > total_time:
+				total_time = en["life_length"]
 		for en in self.entity_list:
 			if en["state"] == 0:
 				continue
@@ -253,7 +278,7 @@ class Entity:
 				"position": [i, j],
 				"value": grid_matrix[i, j]
 			})
-			for row_num in range(i-ENTITY_ABILITY, i+ENTITY_ABILITY+1, 1):
+			for row_num in range(i-en["ability"], i+en["ability"]+1, 1):
 				if row_num == i:
 					continue
 				if row_num > self.po - 1 or row_num < 0:
@@ -262,7 +287,7 @@ class Entity:
 					"position": [row_num, j],
 					"value": grid_matrix[row_num, j]
 				})
-			for col_num in range(j-ENTITY_ABILITY, j+ENTITY_ABILITY+1, 1):
+			for col_num in range(j-en["ability"], j+en["ability"]+1, 1):
 				if col_num == j:
 					continue
 				if col_num > self.po - 1 or col_num < 0:
@@ -288,9 +313,15 @@ class Entity:
 			if en["welfare"] < 0:
 				en["state"] = 0
 		if THRESHOLD_UPPER_BOUND == -1:
-			self.threshold += THRESHOLD_GROW
+			if total_time <= 550:
+				self.threshold += THRESHOLD_GROW
+			else:
+				self.threshold += THRESHOLD_GROW * (((total_time - 550)**(1/2))//1)
 		elif self.threshold < THRESHOLD_UPPER_BOUND:
-			self.threshold += THRESHOLD_GROW
+			if total_time <= 550:
+				self.threshold += THRESHOLD_GROW
+			else:
+				self.threshold += THRESHOLD_GROW * (((total_time - 550)**(1/2))//1)
 
 
 if __name__ == '__main__':
